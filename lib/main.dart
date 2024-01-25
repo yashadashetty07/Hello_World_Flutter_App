8dc4b02012c872dc5b2ba5978f2e21259f2f8545
Yash Adashetty <yash.adashetty@gmail.com>
2024-01-25 14:19:13 +0530

    Hello World Flutter APP

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'MyAPP';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        backgroundColor:Colors.cyanAccent,
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
